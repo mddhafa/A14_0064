2cd4e1f23211524cd9aab7c374e8140f76265a4d
mddhafa <mdmuhammaddhafa1234@gmail.com>
2025-01-26 12:34:32 +0700

    Menambahkan pelanggan di app container

--- app/src/main/java/com/example/villa/container/VillaContainer.kt
+++ app/src/main/java/com/example/villa/container/VillaContainer.kt
@@ -1,7 +1,16 @@
 package com.example.villa.container
 
+import com.example.villa.repository.NetworkPelangganRepository
+import com.example.villa.repository.NetworkReservasiRepository
+import com.example.villa.repository.NetworkReviewRepository
 import com.example.villa.repository.NetworkVillaRepository
+import com.example.villa.repository.PelangganRepository
+import com.example.villa.repository.ReservasiVillaRepository
+import com.example.villa.repository.ReviewRepository
 import com.example.villa.repository.VillaRepository
+import com.example.villa.service_api.PelangganService
+import com.example.villa.service_api.ReservasiService
+import com.example.villa.service_api.ReviewService
 import com.example.villa.service_api.VillaService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
@@ -10,6 +19,8 @@ import retrofit2.Retrofit
 
 interface AppContainer {
     val villaRepository: VillaRepository
+    val pelangganRepository: PelangganRepository
+
 }
 
 class VillaContainer : AppContainer{
@@ -23,9 +34,19 @@ class VillaContainer : AppContainer{
     private val villaService: VillaService by lazy {
         retrofit.create(VillaService::class.java)
     }
+    private val pelangganService: PelangganService by lazy {
+        retrofit.create(PelangganService::class.java)
+    }
+
 
     override val villaRepository: VillaRepository by lazy {
         NetworkVillaRepository(villaService)
     }
+
+    override val pelangganRepository: PelangganRepository by lazy {
+        NetworkPelangganRepository(pelangganService)
+    }
+
+
 
 }
