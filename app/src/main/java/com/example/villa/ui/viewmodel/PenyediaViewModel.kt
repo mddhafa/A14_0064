d39feae61e4d6e5c835b4896a19574f337ecf3e3
mddhafa <mdmuhammaddhafa1234@gmail.com>
2025-01-26 12:44:34 +0700

    Menambahkan initializer masing" viewmodel

--- app/src/main/java/com/example/villa/ui/viewmodel/PenyediaViewModel.kt
+++ app/src/main/java/com/example/villa/ui/viewmodel/PenyediaViewModel.kt
@@ -7,6 +7,18 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.villa.ReservasiVillaApplications
+import com.example.villa.repository.PelangganRepository
+import com.example.villa.ui.viewmodel.pelanggan.DetailPelangganViewModel
+import com.example.villa.ui.viewmodel.pelanggan.HomePelangganViewModel
+import com.example.villa.ui.viewmodel.pelanggan.InsertPelangganViewModel
+import com.example.villa.ui.viewmodel.pelanggan.UpdatePelangganViewModel
+import com.example.villa.ui.viewmodel.reservasi.DetailReservasiViewModel
+import com.example.villa.ui.viewmodel.reservasi.HomeReservasiViewModel
+import com.example.villa.ui.viewmodel.reservasi.InsertReservasiViewModel
+import com.example.villa.ui.viewmodel.reservasi.UpdateReservasiViewModel
+import com.example.villa.ui.viewmodel.review.HomeReviewViewModel
+import com.example.villa.ui.viewmodel.review.InsertReviewViewModel
+import com.example.villa.ui.viewmodel.review.UpdateReviewViewModel
 import com.example.villa.ui.viewmodel.villa.DetailVillaViewModel
 import com.example.villa.ui.viewmodel.villa.HomeVillaViewModel
 import com.example.villa.ui.viewmodel.villa.InsertVillaViewModel
@@ -14,7 +26,12 @@ import com.example.villa.ui.viewmodel.villa.UpdateVillaViewModel
 
 object PenyediaViewModel{
     val Factory = viewModelFactory {
+        //Villa
         initializer { HomeVillaViewModel(aplikasiVilla().container.villaRepository)}
+        initializer { InsertVillaViewModel(aplikasiVilla().container.villaRepository)}
+        initializer { DetailVillaViewModel(createSavedStateHandle(),aplikasiVilla().container.villaRepository) }
+        initializer { UpdateVillaViewModel(createSavedStateHandle(),aplikasiVilla().container.villaRepository) }
+
 
 }
 
