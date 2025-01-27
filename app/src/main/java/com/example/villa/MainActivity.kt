140da03ee86852e35b2eed65eeaea6c6de68d5d7
mddhafa <mdmuhammaddhafa1234@gmail.com>
2025-01-27 14:10:34 +0700

    finish

--- app/src/main/java/com/example/villa/MainActivity.kt
+++ app/src/main/java/com/example/villa/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.villa.ui.navigation.PengelolaHalaman
 import com.example.villa.ui.theme.VillaTheme
 import com.example.villa.ui.viewmodel.ReservasiVillaApp
 
